29012508156ffea2f60dd5e26054ae486690fe1e
shivaganeshtechis <shivaganesh@techis.io>
2021-10-28 09:42:36 +0530

    updated a change

--- frontend/src/containers/Shipping.jsx
+++ frontend/src/containers/Shipping.jsx
@@ -89,104 +89,108 @@ const Shipping = () => {
       </div>
 
       <section class="details">
-        <h3>Shipment Details</h3>
-        <h4>Please check your items and confirm it</h4>
-        <div class="box2">
-          <table>
-            {carts &&
-              carts.map((cart) => (
-                <tr>
-                  <td>{cart.item.name}</td>
-                  <td>{cart.quantity}</td>
-                  <td>{cart.item.Price}</td>
-                </tr>
-              ))}
-            <tr class="border">
-              <td>Total Price</td>
-              <td>{totalitem}</td>
-              <td>${subtotal}</td>
-            </tr>
-          </table>
+        <div class="product-details">
+          <h3>Shipment Details</h3>
+          <h4>Please check your items and confirm it</h4>
+          <div class="box2">
+            <table>
+              {carts &&
+                carts.map((cart) => (
+                  <tr>
+                    <td class="td-item">{cart.item.name}</td>
+                    <td class="td-quantity">{cart.quantity}</td>
+                    <td class="td-price">{cart.item.Price}</td>
+                  </tr>
+                ))}
+              <tr class="border">
+                <td>Total Price</td>
+                <td>{totalitem}</td>
+                <td>${subtotal}</td>
+              </tr>
+            </table>
+          </div>
         </div>
 
-        <div class="form">
-          <label for="name">Full name</label> <br />
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-            placeholder="Enter Recipient's name"
-            onChange={inputFullname}
-          />
-          <br />
-          <label for="number">Phone Number</label> <br />
-          <input
-            type="text"
-            id="number"
-            name="number"
-            required
-            placeholder="Enter Phone Number"
-            onChange={inputPhoneNumber}
-          />
-          <br />
-          <label for="address">Street address or P.O. Box</label> <br />
-          <input
-            type="text"
-            id="address"
-            name="address"
-            required
-            placeholder="Enter Street address or P.O. Box"
-            onChange={inputAddress}
-          />
-          <br />
-          <label for="zip">PIN code</label> <br />
-          <input
-            type="text"
-            id="zip"
-            name="zip"
-            required
-            placeholder="Enter PIN code"
-            onChange={inputPin}
-          />
-          <br />
-          <label for="house">Apt, suite, unit, building, floor, etc.</label>
-          <br />
-          <input
-            type="text"
-            id="house"
-            name="house"
-            required
-            placeholder="Enter Apt, suite, unit, building, floor, etc."
-            onChange={inputHouse}
-          />
-          <br />
-          <label for="city">City</label> <br />
-          <input
-            type="text"
-            id="city"
-            name="city"
-            required
-            placeholder="Enter City"
-            onChange={inputCity}
-          />
-          <br />
-          <label for="state">State</label> <br />
-          <input
-            type="text"
-            id="State"
-            name="state"
-            required
-            placeholder="Enter State"
-            onChange={inputState}
-          />
-          <input
-            type="submit"
-            name="submit"
-            value="SUBMIT"
-            class="submit"
-            onClick={orderButton}
-          ></input>
+        <div class="dispatch">
+          <div class="form">
+            <label for="name">Full name</label> <br />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              placeholder="Enter Recipient's name"
+              onChange={inputFullname}
+            />
+            <br />
+            <label for="number">Phone Number</label> <br />
+            <input
+              type="text"
+              id="number"
+              name="number"
+              required
+              placeholder="Enter Phone Number"
+              onChange={inputPhoneNumber}
+            />
+            <br />
+            <label for="address">Street address or P.O. Box</label> <br />
+            <input
+              type="text"
+              id="address"
+              name="address"
+              required
+              placeholder="Enter Street address or P.O. Box"
+              onChange={inputAddress}
+            />
+            <br />
+            <label for="zip">PIN code</label> <br />
+            <input
+              type="text"
+              id="zip"
+              name="zip"
+              required
+              placeholder="Enter PIN code"
+              onChange={inputPin}
+            />
+            <br />
+            <label for="house">Apt, suite, unit, building, floor, etc.</label>
+            <br />
+            <input
+              type="text"
+              id="house"
+              name="house"
+              required
+              placeholder="Enter Apt, suite, unit, building, floor, etc."
+              onChange={inputHouse}
+            />
+            <br />
+            <label for="city">City</label> <br />
+            <input
+              type="text"
+              id="city"
+              name="city"
+              required
+              placeholder="Enter City"
+              onChange={inputCity}
+            />
+            <br />
+            <label for="state">State</label> <br />
+            <input
+              type="text"
+              id="State"
+              name="state"
+              required
+              placeholder="Enter State"
+              onChange={inputState}
+            />
+            <input
+              type="submit"
+              name="submit"
+              value="SUBMIT"
+              class="submit"
+              onClick={orderButton}
+            ></input>
+          </div>
         </div>
       </section>
     </>
