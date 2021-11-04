0979d66b078a81adb9396f27db81c083e9c2d6fb
shivaganeshtechis <shivaganesh@techis.io>
2021-11-04 23:15:31 +0530

    updated files

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,14 +2,15 @@ import axios from "axios";
 const LOGIN_USER_KEY = "LOGIN_USER_KEY";
 
 var baseURL;
-if (
-  process.env.REACT_APP_ENVIRONMENT &&
-  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
-) {
-  baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-  baseURL = "http://127.0.0.1:8000";
-}
+// if (
+//   process.env.REACT_APP_ENVIRONMENT &&
+//   process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+// ) {
+//   baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+//   baseURL = "http://127.0.0.1:8000";
+// }
+baseURL = "https://backend-electric-views.herokuapp.com/";
 
 const api = axios.create({
   baseURL: baseURL,
